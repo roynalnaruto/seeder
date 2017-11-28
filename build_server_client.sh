20afa67b86cdb12e8b9353955c19002248d1e5fd
rohit_n <narurkar_r@worksap.co.jp>
2017-11-28 14:28:57 +0800

    edit readme

--- build_server_client.sh
+++ build_server_client.sh
@@ -1,2 +1,2 @@
-g++ server.cpp -lboost_system -lboost_thread -lpthread -std=c++11 -o server
-g++ client.cpp -lboost_system -lboost_thread -lpthread -std=c++11 -o client
+g++ server.cpp -lboost_system -lboost_thread-mt -std=c++11 -stdlib=libc++ -o server
+g++ client.cpp -lboost_system -lboost_thread-mt -std=c++11 -stdlib=libc++ -o client
